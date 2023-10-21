b38dd96decf47470ee4b73f91a863d8ad50d3e12
Malik Malope <140521344+libSpace@users.noreply.github.com>
2023-10-21 11:21:49 +0200

    form

--- packages/js/validate.js
+++ packages/js/validate.js
@@ -1,14 +1,26 @@
 //Validation
 function validate(){
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const subject = document.getElementById("subject").value;
-    const feed = document.getElementById("feed").value;
-     if(name == ""|| email == "" || subject == "" || feed ==""){
-        alert("The form is not fully filled!");
-        // return false;
-     }else{
-        alert("nice");
-        // return true;
-     }
+   const name = document.getElementById("name").value;
+   const email = document.getElementById("email").value;
+   const subject = document.getElementById("subject").value;
+   const feed = document.getElementById("feed").value;
+  
+   function validateEmail(email) {
+      // Regular expression for matching email addresses
+      var pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+      return pattern.test(email);
+    }
+
+   if(name == ""|| email == "" || subject == "" || feed ==""){
+      alert("The form is not fully filled!");
+      return false;
+   }else if(typeof name !== 'string' && !/^[A-Za-z]+$/.test(name)){
+      
+   }
+   else if(!validateEmail(email)){
+      alert("Check your email")
+   }else{
+      alert("nice");
+      return true;
+   }
 }
