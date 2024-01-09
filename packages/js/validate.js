3eff6f636df545df6df7e2d90494937eeaf6f3b9
Malik Malope <140521344+libSpace@users.noreply.github.com>
2024-01-09 14:27:49 +0200

    Testimonials

--- packages/js/validate.js
+++ packages/js/validate.js
@@ -1,24 +1,24 @@
 //Validation
-function validate(){
-   const name = document.getElementById("name").value;
-   const email = document.getElementById("email").value;
-   const subject = document.getElementById("subject").value;
-   const feed = document.getElementById("feed").value;
-  
-   function validateEmail(email) {
-      // Regular expression for matching email addresses
-      var pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-      return pattern.test(email);
+function validate() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const feed = document.getElementById("feed").value;
+
+    function validateEmail(email) {
+        // Regular expression for matching email addresses
+        var pattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        return pattern.test(email);
     }
 
-   if(name == ""|| email == "" || subject == "" || feed ==""){
-      alert("The form is not fully filled!");
-      return false;
-   }
-   else if(!validateEmail(email)){
-      alert("Check your email")
-   }else{
-      alert("Success");
-      return true;
-   }
+    if (name === "" || email === "" || subject === "" || feed === "") {
+        alert("The form is not fully filled!");
+        return false;
+    } else if (!validateEmail(email)) {
+        alert("Check your email");
+        return false;
+    } else {
+        alert("Success");
+        return true;
+    }
 }
